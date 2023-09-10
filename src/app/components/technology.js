f733d8a1189ac4d29f163397e35a9ea6a9a7d2dd
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-09-10 19:05:38 -0300

    Update

--- src/app/components/technology.js
+++
@@ -1,9 +0,0 @@
-export default function Technology() {
-  return (
-    <div className='w-full flex items-center px-3 py-8 gap-4 border-l-2 border-[#636d72] hover:border-l-2 hover:border-[#5477ff] duration-100 hover:text-white text-gray-400 '>
-
-      <SiDart className='h-12 w-12 duration-150' />
-      <p className='mt-2'>Dart</p>
-    </div>
-  );
-}
