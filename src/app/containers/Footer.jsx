6bda4a58142e47bfb87e1128f8fb8f2947c5726a
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-09-11 01:13:27 -0300

    footer

--- src/app/containers/Footer.jsx
+++ src/app/containers/Footer.jsx
@@ -8,10 +8,10 @@ export default function Footer() {
     <footer className='mt-24 flex border-t-2 bg-[#00000044] border-[#636d726d] text-center justify-center flex-col items-center h-56 w-full'>
       <p>© 2023. Lautaro Lazzeri</p>
       <div className='flex items-center mt-8 mb-8'>
-              <Link href='/'><FiInstagram       className='text-gray-400 hover:text-white duration-150 h-8 w-8' /> </Link>
-              <Link href='/'><AiOutlineLinkedin className='text-gray-400 hover:text-white duration-150 h-8 w-8 ml-3' /> </Link>
-              <Link href='/'><AiOutlineGithub   className='text-gray-400 hover:text-white duration-150 h-8 w-8 ml-3' /> </Link>
-              <Link href='/'><HiOutlineMail     className='text-gray-400 hover:text-white duration-150 h-8 w-8 ml-3' /> </Link>
+      <Link href='https://www.instagram.com/lautarolazzeri' target="_blank"><FiInstagram       className='text-gray-400 hover:text-white duration-150 h-8 w-8' /> </Link>
+              <Link href='https://www.linkedin.com/in/lautaro-lazzeri/' target="_blank"><AiOutlineLinkedin className='text-gray-400 hover:text-white duration-150 h-8 w-8 ml-3' /> </Link>
+              <Link href='https://www.github.com/lautarolazzeri' target="_blank"><AiOutlineGithub   className='text-gray-400 hover:text-white duration-150 h-8 w-8 ml-3' /> </Link>
+              <Link href='mailto:lautyylazzerii@gmail.com'><HiOutlineMail     className='text-gray-400 hover:text-white duration-150 h-8 w-8 ml-3' /> </Link>
             </div>
 
     </footer>
